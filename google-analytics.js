e7e84c5704092272894c1d2385d0b243d102cf06
viktorbarzin <vbarzin@gmail.com>
2019-03-26 23:55:23 +0000

    extract ga script in separate file

---
+++ google-analytics.js
@@ -0,0 +1,5 @@
+<script type="application/javascript">
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+ga('create', 'UA-132992428-1', 'auto');
+ga('send', 'pageview');
+</script>
